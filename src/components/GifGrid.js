4724ab01fccca003447e4fb0ee55fb93b112f3fc
jmoragarios <jmoragarios@gmail.com>
2021-02-18 21:27:31 -0600

    Added GifGriditem component

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
+    const [images, setImages] = useState([])
+
+    useEffect( () => {
+        getGifs();
+    }, []);
+
     const getGifs = async () => {
         
-        const url = 'http://api.giphy.com/v1/gifs/search?q=Naruto&limit=10&api_key=F5av68ecrjZl4tx7XtSN679Z8HVx91Bl';
+        const url = 'http://api.giphy.com/v1/gifs/search?q=One%20Piece&limit=10&api_key=F5av68ecrjZl4tx7XtSN679Z8HVx91Bl';
         const response = await fetch( url );
         const { data } = await response.json();
         const gifs = data.map( img => {
@@ -16,14 +23,22 @@ export const GifGrid = ({ category }) => {
         })
         
         console.log(gifs);
-
+        setImages(gifs);
     }
-
-    getGifs();
 
     return (
         <>
             <h3>{ category }</h3>
+
+            {
+                images.map( img => (
+                    <GifGridItem
+                        key={img.id}
+                        {...img}    
+                    />
+                ))
+            }
+            
         </>
     )
 }
