f26ee53ead9eb908637ff9a9e76dbebb9e3eabd3
jmoragarios <jmoragarios@gmail.com>
2021-02-21 21:11:30 -0600

    Added proptypes to GifGridItem component

--- src/tests/Components/GifGridItem.test.js
+++ src/tests/Components/GifGridItem.test.js
@@ -3,9 +3,13 @@ import { shallow } from 'enzyme';
 import { GifGridItem } from '../../components/GifGridItem';
 
 describe('Tests in <GifGriditem />', () => {
+
+    const title = 'dummy title';
+    const url = 'http://localhost/algo.png';
+
     test('should show the component successfuly', () => {
 
-        const wrapper = shallow( <GifGridItem/ >)
+        const wrapper = shallow( <GifGridItem title= { title } url={ url }/ >)
 
         expect( wrapper ).toMatchSnapshot();
     })
